4198ef1a90bd7eb76060610e58c374dadf332d1f
Amina942 <amina.benbadra@yahoo.com>
2024-09-17 18:35:28 +0200

    Add files via upload

--- MINI PROJET GESTION COMPTE BANCAIRE/Compte.cpp
+++ MINI PROJET GESTION COMPTE BANCAIRE/Compte.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+//#include <cmath>
 #include "Compte.h"
 using namespace std;
 Compte::Compte(char *id,char *N,char *P,double num,double sld,char *rib)
@@ -33,22 +33,42 @@ float Compte::Credit()
        {
             cout<<"\n Erreur \n\n Votre solde est : "<<solde<<endl;
        }
+       choix = 0;
+       system("pause");
     }
-    
-
+    else if (choix == 2)
+    {
+        system("CLS");
+        cout<<" Crediter un compte existant "<<endl;
+        cout<<"\t\n Quel compte voulez vous creditez ? Entrer le Nom \n\n";
+        string autre ;
+        cin>>autre;
+        system("CLS");
+        cout<<"\t\n Combien voulez vous credite ? \n\n";
+        cin>>choix;
+        if (choix>=0)
+        {
+            system("CLS");
+            solde=solde+choix;
+            cout<<"\n Solde ajouter\n\n Votre solde s'eleve  a"<<solde<<"e\n";
+        }
+        else
+        {
+            system("CLS");
+            cout<<"\n Erreur \n\n Votre solde est : "<<solde<<endl;
+        }
+        system("CLS");
+        
+    }
+    return 0;
 }
 float Compte::Debit()
 {
     system("CLS");
-    cout<<"  1: Retirer de l'argent"<<endl;
-    cout<<"  2: Retirer de l'argent d'un autre compte"<<endl;
-    unsigned int choix;
+    cout<<"\n\t Debiter"<<endl;
+    cout<<" Combien voulez vous retirer ?"<<endl;
+    int choix;
     cin>>choix;
-    if (choix>=1)
-    {
-        cout<<"\n\tRetirer de l'argent\n\n";
-        cout<<"\t\nMontant a retirer : \n\n";
-        cin>>choix;
         if (choix>=0)
         {
             solde=solde-choix;
@@ -58,64 +78,101 @@ float Compte::Debit()
         {
             cout<<"\n Erreur \n\n Votre solde est : "<<solde<<endl;
         }
-    }
+        choix = 0;
+        return 0;
+    
     
 
 }
+
 float Compte::Consulter()
 {
     system("CLS");
+    cout<<"\n\t Consulter les information du compte"<<endl;
     cout<<"  1: Consulter le solde"<<endl;
     cout<<"  2: Consulter un RIB"<<endl;
-    unsigned int choix;
+    cout<<"Retour\n\n";
+    int choix;
     cin>>choix;
-    if (choix>=1)
+    switch (choix)
     {
-        cout<<"\n\tConsulter le solde\n\n";
-        cout<<"\n Votre solde est : "<<solde<<endl;
-        cin>>choix;
-        if (choix>=0)
-        {
-            cout<<"\n\tConsulter un RIB\n\n";
-            cout<<"  RIB : "<<RIB<<endl;
-        }
-        else
-        {
-            cout<<"\n Erreur \n\n Votre solde est : "<<solde<<endl;
-        }
+    case 1:
+        system("CLS");
+        cout<<"\n\t Consulter le solde"<<endl;
+        cout<<"  Votre solde est : "<<solde<<endl;
+        system("pause");
+        break;
+    case 2:
+        system("CLS");
+        cout<<"\n\t Consulter un RIB"<<endl;
+        cout<<"  RIB : "<<RIB<<endl;
+        system("pause");
+        break;
+    default:
+        break;
     }
 }
 float Compte::Virement()
 {
     system("CLS");
-    cout<<"  1: Virement a un compte existant"<<endl;
-    cout<<"  2: Virement a un compte inexistant"<<endl;
-    unsigned int choix;
-    cin>>choix;
-    if (choix>=1)
+    cout<<" Quel compte voulez vous creditez"<<endl;
+    string autre;
+    cin>>autre;
+    system("CLS");
+    cout<<"Montznt du virement : ";
+    double choixV;
+    cin>>choixV;
+    if (choixV>=0)
     {
-        cout<<"\n\tVirement a un compte existant\n\n";
-        cout<<"\n Votre solde est : "<<solde<<endl;
-        cout<<"\n Votre RIB est : "<<RIB<<endl;
-        cin>>choix;
-     if (choix>=0)
-    {
-        cout<<"\n\tVirement a un compte inexistant\n\n";
-        cout<<"\n Votre solde est : "<<solde<<endl;
-        cout<<"\n Votre RIB est : "<<RIB<<endl;
+        system("CLS");
+        solde=solde-choixV;
+        cout<<" Virement effectue avec succes"<<endl;
+        cout<<" Votre solde est : "<<solde<<endl;
     }
     else
     {
         cout<<"\n Erreur \n\n Votre solde est : "<<solde<<endl;
     }
-    }
-}
-float Compte::Commander()
-{
-    system("CLS");
-    cout<<"  1: Commander un produit"<<endl;
+    choixV = 0;
+    system("pause");
 }
 void Compte::Afficher()
 {
-    cout<<"\n\tAfficher les informations du compte\n\n";
+    int cpt=1;
+    while (cpt !=0)
+    {
+        system("CLS");
+        cout<<"\n\t Menu"<<endl;
+        cout<<"1. Crediter votre compte"<<nom<<" "<<prenom<<endl;
+        cout<<"2. Debiter  votre compte"<<endl;
+        cout<<"3. Virement"<<endl;
+        cout<<"4. Consulter les information du compte"<<endl;
+        cout<<"5. Quitter \n\n choix :  ";
+        int choix;
+        cin>>choix;
+        switch (choix)
+        {
+        case 1:
+            Credit();
+            break;
+        case 2:
+            Debit();
+            break;
+        case 3:
+            Virement();
+            break;
+        case  4:
+            Consulter();
+            break;
+        case  5:
+            cpt=0;
+            break;
+        default:
+            system("CLS");
+            cout<<"\n Erreur \n\n ";
+            choix=0;
+            system("pause");
+            break;
+        }
+    }
 }
--- MINI PROJET GESTION COMPTE BANCAIRE/main.cpp
+++ MINI PROJET GESTION COMPTE BANCAIRE/main.cpp
@@ -5,6 +5,32 @@
 using namespace std;
 main()
 {
-    Compte Compte();
+    system("CLS");
+    Compte Compte("FR-AAAC91711G210","BENBADRA","Amina",123456789,100,"FR76 3000 4028 3798  0000 0123 4567");
+    int cpt=0;
+    while (cpt!=3)
+    {
+        system("CLS");
+        cout<<"\n\tEntrer le code de securite : ";
+        int code;
+        cin>>code;
+        if (code==1234)
+        {
+            system("CLS");
+            cout<<"\n\tBienvenue\n\n\n";
+            system("pause");
+            Compte.Afficher();
+            return 0;
+        }
+        else
+        {
+            system("CLS");
+            cout<<"\n\tCode de securite incorrect\n\n\n";
+            system("pause");
+            cpt++;
+        }
+        return 0;
+    }
+
 
 }
