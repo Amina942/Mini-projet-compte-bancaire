13db9901dac06df5eeb712281794cd41129f034e
Amina942 <amina.benbadra@yahoo.com>
2024-09-19 12:53:39 +0200

    Add files via upload

--- MINI PROJET GESTION COMPTE BANCAIRE/Clien.cpp
+++ MINI PROJET GESTION COMPTE BANCAIRE/Clien.cpp
@@ -3,3 +3,14 @@
 #include "Clien.h"
 #include "Compte.h"
 using namespace std;
+
+Clien::Clien(char *id,char *N,char *P,double num,double sld)
+{
+    identifiant = id;
+    nom = N;
+    prenom = P;
+    numero = num;
+    solde = sld;
+}
+
+
--- MINI PROJET GESTION COMPTE BANCAIRE/Clien.h
+++ MINI PROJET GESTION COMPTE BANCAIRE/Clien.h
@@ -4,5 +4,13 @@ using namespace std;
 class Clien
 {
     private:
+    char *identifiant;
+    char *nom;
+    char *prenom;
+    double numero;
+    double solde;
     public:
+    Clien(char *,char *,char *,double,double);
+    float Consulter();
+    void Afficher();
 };
